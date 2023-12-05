230bc8bb3bb63ae0871d8a8d6ef0eb704eddd755
bakkeshks <bakkeshpruthvi49@gmail.com>
2023-12-06 03:28:31 +0530

    Completed: palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,13 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  var len = str.length;
+  for (var i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
   return true;
 }
 
