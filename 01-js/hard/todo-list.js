43232fdfb37c8e37f40884b3032dafe563785fc6
bakkeshks <bakkeshpruthvi49@gmail.com>
2023-12-08 01:35:26 +0530

    Completed: to-do list

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -9,9 +9,46 @@
 
   Once you've implemented the logic, test your code by running
 */
-
 class Todo {
+  constructor(){
+    this.obj = [];
+    this.i = 0;
+  }
 
+  add(todo){
+    this.obj[this.i] = todo;
+    this.i++;
+  }
+
+  remove(indexOfTodo){
+    if(indexOfTodo<0||indexOfTodo>this.i-1){
+      return "Invalid  task";
+    }
+    this.obj.splice(indexOfTodo, 1);
+    this.i--;
+  }
+
+  update(indexOfTodo, updatedTodo){
+    if(indexOfTodo<0||indexOfTodo>this.i-1){
+      return "Invalid  task";
+    }
+    this.obj[indexOfTodo] = updatedTodo;
+  }
+
+  getAll(){
+    return this.obj;
+  }
+
+  get(indexOfTodo){
+    if(indexOfTodo<0||indexOfTodo>this.i-1){
+      return null;
+    }
+    return this.obj[indexOfTodo]
+  }
+
+  clear(){
+    this.obj = [];
+  }
 }
 
 module.exports = Todo;
