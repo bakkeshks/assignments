978fc4e2478b21c4df64a93879a38b4a39ab235f
bakkeshks <bakkeshpruthvi49@gmail.com>
2023-12-27 16:12:45 +0530

    Completed: promise chain

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,34 @@
  */
 
 function wait1(t) {
-
-}
-
-function wait2(t) {
-
-}
-
-function wait3(t) {
-
-}
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), t * 1000); // Delay in milliseconds
+    });
+  }
+  
+  function wait2(t) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), t * 1000);
+    });
+  }
+  
+  function wait3(t) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), t * 1000);
+    });
+  }
 
 function calculateTime(t1, t2, t3) {
+    const startTime = Date.now();
 
+  return wait1(t1)
+    .then(() => wait2(t2))
+    .then(() => wait3(t3))
+    .then(() => Date.now() - startTime)
+    .then((totalTime) => {
+      console.log(`Time taken for sequential promises: ${totalTime} milliseconds`);
+      return totalTime;
+    });
 }
 
 module.exports = calculateTime;
